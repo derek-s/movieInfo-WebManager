0282c95708ca663a63acad62c0ecf34f2ed1facb
Derek.S <derekseli@outlook.com>
2020-03-01 20:46:06 +0800

    main file

---
+++ app.py
@@ -0,0 +1,34 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+# File:app.py
+# File Created:2020-03-01 08:03:28
+# Author:Derek.S(derekseli@outlook.com)
+# -----
+# Last Modified:2020-03-01 08:03:28
+# -----
+
+from flask import Flask
+from flask_pymongo import PyMongo
+from views.index import indexViews
+
+
+from config import SECRET_KEY, dbConfig
+
+app = Flask(__name__, template_folder="templates")
+
+app.secret_key = SECRET_KEY
+
+app.config.update(
+    MONGO_URI=dbConfig["URI"],
+    DEBUG=True
+)
+
+mongo = PyMongo(app)
+
+app.jinja_env.auto_reload = True
+
+app.register_blueprint(indexViews)
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5050)
