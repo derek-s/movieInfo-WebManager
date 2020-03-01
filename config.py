95b70614093d0a1adf64d526e76875dc659e2273
Derek.S <derekseli@outlook.com>
2020-03-01 20:45:59 +0800

    config file

---
+++ config.py
@@ -0,0 +1,14 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+# File:config.py
+# File Created:2020-03-01 08:06:41
+# Author:Derek.S(derekseli@outlook.com)
+# -----
+# Last Modified:2020-03-01 08:06:41
+# -----
+
+SECRET_KEY = "b270a04947531b4987f85c3043f06ee1a3cd10829dbf48ad44d32b2a"
+
+dbConfig = {
+    "URI": "mongodb://mInfodbu:123456@192.168.50.50:27017/movieInfo"
+}
